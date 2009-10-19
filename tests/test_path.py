196a97687b54061d1c011ecdeaf6f9d5390ac4a4
Ed Singleton <singletoned@gmail.com>
2009-10-19 21:56:35 +0100

    Added some more tests

--- tests/test_path.py
+++ tests/test_path.py
@@ -2,11 +2,48 @@ from nose.tools import raises
 
 from path import Path, InsecurePathError
 
+
 def test_repr():
     p = Path("/temp/test_dir")
     print repr(p)
     assert repr(p) == "Path('/temp/test_dir')"
 
+def test__add():
+    p = Path("/tmp/test_dir")
+    r = p + "sub_dir"
+    assert r == "/tmp/test_dirsub_dir"
+    r = p + "/sub_dir"
+    assert r == "/tmp/test_dir/sub_dir"
+    p = Path("/tmp/test_dir/")
+    r = p + "sub_dir"
+    assert r == "/tmp/test_dir/sub_dir"
+    p = Path("/tmp/test_dir/")
+    r = p + "/sub_dir"
+    assert r == "/tmp/test_dir//sub_dir"
+
+def test__radd():
+    p = Path("sub_dir")
+    r = "/tmp/test_dir" + p
+    assert r == "/tmp/test_dirsub_dir"
+    p = Path("/sub_dir")
+    r = "/tmp/test_dir" + p
+    assert r == "/tmp/test_dir/sub_dir"
+    p = Path("sub_dir")
+    r = "/tmp/test_dir/" + p
+    assert r == "/tmp/test_dir/sub_dir"
+    p = Path("/sub_dir")
+    r = "/tmp/test_dir/" + p
+    assert r == "/tmp/test_dir//sub_dir"
+
+def test_cwd():
+    # TODO
+    pass
+
+def test_abspath():
+    # Check that abspath converts to Path
+    p = Path("/tmp/../tmp/test_dir")
+    r = p.abspath()
+    assert r == "/tmp/test_dir"
 
 def test_abs_child():
     p = Path("/tmp/test_dir")
