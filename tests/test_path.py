1e2d977803886489b6fc80e4437cfe60b1c33cfe
Ed Singleton <singletoned@gmail.com>
2009-10-19 22:51:14 +0100

    Added more tests

--- tests/test_path.py
+++ tests/test_path.py
@@ -44,6 +44,60 @@ def test_abspath():
     p = Path("/tmp/../tmp/test_dir")
     r = p.abspath()
     assert r == "/tmp/test_dir"
+    assert isinstance(r, Path)
+
+def test_normcase():
+    # TODO
+    pass
+
+def test_normpath():
+    # Check that normpath runs and converts to Path
+    p = Path("/tmp/..//tmp/test_dir")
+    r = p.normpath()
+    assert r == "/tmp/test_dir"
+    assert isinstance(r, Path)
+
+def test_realpath():
+    # TODO
+    pass
+
+def test_expanduser():
+    # Check that expanduser runs and converts to Path
+    p = Path("/tmp")
+    r = p.expanduser()
+    assert r == p
+    assert isinstance(r, Path)
+    # TODO: Check that it expands `~` properly
+
+def test_expandvars():
+    # TODO
+    pass
+
+def test_expand():
+    # TODO
+    pass
+
+def test_get_namebase():
+    # Test that _get_namebase doesn't return a path
+    p = Path("/tmp/test_file")
+    r = p._get_namebase()
+    assert r == "test_file"
+    assert not isinstance(r, Path)
+    p = Path("/tmp/test.file")
+    r = p._get_namebase()
+    assert r == "test"
+    assert not isinstance(r, Path)
+
+def test_get_ext():
+    # Test that _get_ext doesn't return a path
+    p = Path("/tmp/test_file")
+    r = p._get_ext()
+    assert r == ""
+    assert not isinstance(r, Path)
+    p = Path("/tmp/test.file")
+    r = p._get_ext()
+    assert r == ".file"
+    assert not isinstance(r, Path)
 
 def test_abs_child():
     p = Path("/tmp/test_dir")
