e7d29bdf16549dc001fcd0bb358f8472348f2c37
Ed Singleton <singletoned@gmail.com>
2010-03-10 10:16:56 +0000

    Added a basic setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='forked-path',
+      version=version,
+      author="Ed Singleton",
+      author_email="singletoned@gmail.com",
+      description="An object oriented file path module",
+      url="http://github.com/Singletoned/forked-path",
+      long_description="""A fork of path.py by Jason Orendorff.  Regression tests and small enhancements by Ed Singleton.
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      license='',
+      # packages=find_packages('lib'),
+      # package_data=PackageData,
+      # package_dir = {'': 'lib'},
+      # packages = ["path"],
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
