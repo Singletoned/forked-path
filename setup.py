50e69a484093d57484df46c53616eecf05dcd0f9
Ed Singleton <singletoned@gmail.com>
2010-05-26 09:38:07 +0100

    Fixed indentation in setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@ from setuptools import setup, find_packages
 
 version = '0.1'
 
-setup(name='forked-path',
-      version=version,
-      author="Ed Singleton",
-      author_email="singletoned@gmail.com",
-      description="An object oriented file path module",
-      url="http://github.com/Singletoned/forked-path",
-      long_description="""A fork of path.py by Jason Orendorff.  Regression tests and small enhancements by Ed Singleton.
+setup(
+    name='forked-path',
+    version=version,
+    author="Ed Singleton",
+    author_email="singletoned@gmail.com",
+    description="An object oriented file path module",
+    url="http://github.com/Singletoned/forked-path",
+    long_description="""A fork of path.py by Jason Orendorff.  Regression tests and small enhancements by Ed Singleton.
 """,
-      classifiers=[
+    classifiers=[
         "Programming Language :: Python",
         "License :: Freely Distributable",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='path path.py file',
-      license='',
-      py_modules=['path'],
-      include_package_data=True,
-      zip_safe=False,
+    ],
+    keywords='path path.py file',
+    license='',
+    py_modules=['path'],
+    include_package_data=True,
+    zip_safe=False,
 )
